2da91a0e56bd732f9df828b529ddf41a33cff640
sampoyigi <sam@sampoyigi.com>
2016-12-22 12:29:29 +0100

    Remove extensionMeta function, use meta file instead and fix spelling error.

--- Extension.php
+++ Extension.php
@@ -5,17 +5,6 @@ if (!defined('BASEPATH')) exit('No direct access allowed');
 class Extension extends \Base_Extension
 {
 
-	public function extensionMeta() {
-		return array(
-			'code'        => 'cart_module',
-			'name'       => 'Cart',
-			'description' => 'Easily add a shopping cart to your site. The most powerful way to sell your menu items.',
-			'author'      => 'SamPoyigi',
-			'icon'        => 'fa-shopping-cart',
-			'version'     => '2.0',
-		);
-	}
-
 	public function registerComponents() {
 		return array(
 			'cart_module/components/Cart_module' => array(
