19f6ddd8cc803a3f2123f9135bded604847b3b13
SamPoyigi <sam@sampoyigi.com>
2019-11-24 00:38:46 +0000

    Use custom radio buttons to list payments instead

--- components/checkout/payments.php
+++ components/checkout/payments.php
@@ -7,30 +7,37 @@
                 value="">
             <div class="form-group">
                 <label for=""><?= lang('igniter.cart::default.checkout.label_payment_method'); ?></label><br/>
-                <div class="btn-group btn-group-toggle btn-group-vertical w-100" data-toggle="buttons">
+                <div class="list-group">
                     <?php foreach ($paymentGateways as $paymentGateway) { ?>
-                        <div
-                            class="btn btn-light text-left<?= set_value('payment') == $paymentGateway->code ? ' active' : ''; ?>"
-                            role="button">
-                            <input
-                                type="radio"
-                                name="payment"
-                                value="<?= $paymentGateway->code ?>"
-                                autocomplete="off"
-                                <?= set_radio('payment', $paymentGateway->code) ?>
-                            />
-                            <?= $paymentGateway->name; ?>
-                            <?php if (!$paymentGateway->isApplicable($order->order_total, $paymentGateway)) { ?>
-                                <p class="text-info mb-0">
-                                    <?= sprintf(
-                                        lang('igniter.payregister::default.alert_min_order_total'),
-                                        currency_format($paymentGateway->order_total),
-                                        $paymentGateway->name
-                                    ); ?>
-                                </p>
-                            <?php } ?>
+                        <div class="list-group-item">
+                            <div
+                                class="custom-control custom-radio<?= set_value('payment') == $paymentGateway->code ? ' active' : ''; ?>"
+                            >
+                                <input
+                                    type="radio"
+                                    id="payment-<?= $paymentGateway->code ?>"
+                                    class="custom-control-input"
+                                    name="payment"
+                                    value="<?= $paymentGateway->code ?>"
+                                    autocomplete="off"
+                                    <?= set_radio('payment', $paymentGateway->code) ?>
+                                />
+                                <label
+                                    class="custom-control-label d-block"
+                                    for="payment-<?= $paymentGateway->code ?>"
+                                ><?= $paymentGateway->name; ?></label>
+                                <?php if (!$paymentGateway->isApplicable($order->order_total, $paymentGateway)) { ?>
+                                    <p class="text-info font-weight-normal mb-0">
+                                        <?= sprintf(
+                                            lang('igniter.payregister::default.alert_min_order_total'),
+                                            currency_format($paymentGateway->order_total),
+                                            $paymentGateway->name
+                                        ); ?>
+                                    </p>
+                                <?php } ?>
+                            </div>
+                            <?= $paymentGateway->renderPaymentForm($this->controller); ?>
                         </div>
-                        <?= $paymentGateway->renderPaymentForm($this->controller); ?>
                     <?php } ?>
                 </div>
                 <?= form_error('payment', '<span class="text-danger">', '</span>'); ?>
