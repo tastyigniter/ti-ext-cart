1db05fa51d610e28f2a96b4f75072458b38bb61c
Ryan Mitchell <ryan@thoughtcollective.com>
2020-09-25 00:20:25 +0100

    Use first mealtime when throwing error text (#25)

--- classes/CartManager.php
+++ classes/CartManager.php
@@ -284,13 +284,20 @@ class CartManager
     {
         // if menu mealtime is enabled and menu is outside mealtime
         if (!$menuItem->isAvailable()) {
-            throw new ApplicationException(sprintf(
-                lang('igniter.cart::default.alert_menu_not_within_mealtime'),
-                $menuItem->menu_name,
-                $menuItem->mealtime->mealtime_name,
-                $menuItem->mealtime->start_time,
-                $menuItem->mealtime->end_time
-            ));
+            throw new ApplicationException(
+                sprintf(
+                    lang('igniter.cart::default.alert_menu_not_within_mealtimes'),
+                    $menuItem->menu_name,
+                    $menuItem->mealtimes->map(function ($mealtime) {
+                        return sprintf(
+                            lang('igniter.cart::default.alert_menu_not_within_mealtimes_option'),
+                            $mealtime->mealtime_name,
+                            $mealtime->start_time,
+                            $mealtime->end_time
+                        );
+                    })->join(', ')
+                )
+            );
         }
 
         if ($menuItem->hasOrderTypeRestriction($orderType = $this->location->orderType())) {
