4844ad08d640b15f500b58b6f5baf45778826db2
Sam Poyigi <sam@sampoyigi.com>
2021-02-22 23:45:11 +0000

    put back @buttons partial

--- components/cartbox/mobile.blade.php
+++ components/cartbox/mobile.blade.php
@@ -3,6 +3,7 @@
     class="{{ !$pageIsCheckout ? 'fixed-bottom' : 'mt-3' }}{{ $pageIsCart ? 'hide' : ' d-block d-lg-none' }}"
 >
     @if ($pageIsCheckout)
+        @partial('@buttons')
     @elseif (!$pageIsCart)
         <a
             class="btn btn-primary btn-block btn-lg radius-none cart-toggle text-nowrap"
