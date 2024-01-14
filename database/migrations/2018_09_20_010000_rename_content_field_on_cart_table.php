fd7b19450d786c0ac039573b55743d8ca6950cff
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-14 23:50:31 +0000

    Minor refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- database/migrations/2018_09_20_010000_rename_content_field_on_cart_table.php
+++ database/migrations/2018_09_20_010000_rename_content_field_on_cart_table.php
@@ -7,10 +7,13 @@ use Illuminate\Support\Facades\Schema;
 /**
  * Rename content column
  */
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
+        if (!Schema::hasColumn('igniter_cart_cart', 'content')) {
+            return;
+        }
+
         Schema::table('igniter_cart_cart', function (Blueprint $table) {
             $table->renameColumn('content', 'data');
         });
