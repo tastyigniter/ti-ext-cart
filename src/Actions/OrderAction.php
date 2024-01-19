346c41980b975be3f141aa55ead8e19bdbe0d038
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-19 23:14:30 +0000

    Refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Actions/OrderAction.php
+++ src/Actions/OrderAction.php
@@ -2,16 +2,13 @@
 
 namespace Igniter\Cart\Actions;
 
-use Igniter\Flame\Database\Model;
 use Igniter\System\Actions\ModelAction;
 
 class OrderAction extends ModelAction
 {
-    protected Model $model;
-
     public function __construct($model)
     {
-        $this->model = $model;
+        parent::__construct($model);
 
         $this->model->fillable(['customer_id', 'address_id', 'first_name', 'last_name', 'email', 'telephone', 'comment', 'delivery_comment', 'payment']);
     }
