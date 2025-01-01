19638975f2c6fd8672114865a1e107608ea299b5
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-01-01 03:27:20 +0000

    chore: add method annotations for facades
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Facades/Cart.php
+++ src/Facades/Cart.php
@@ -4,6 +4,36 @@ namespace Igniter\Cart\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Igniter\Cart\Cart instance(string|null $instance = null)
+ * @method static string currentInstance()
+ * @method static array|\Igniter\Cart\CartItem add(void $buyable, int $qty = 0, array $options = [], void $comment = null)
+ * @method static \Igniter\Cart\CartItem|bool update(string $rowId, mixed $qty)
+ * @method static void remove(string $rowId)
+ * @method static \Igniter\Cart\CartItem get(string $rowId)
+ * @method static void destroy(mixed $identifier = null)
+ * @method static \Igniter\Cart\CartContent content()
+ * @method static int count()
+ * @method static string total()
+ * @method static float subtotal()
+ * @method static \Igniter\Cart\CartContent search(\Closure $search)
+ * @method static void associate(string $rowId, mixed $model)
+ * @method static \Igniter\Cart\CartConditions conditions()
+ * @method static void conditionsWithoutApplied()
+ * @method static \Igniter\Cart\CartCondition getCondition(void $name)
+ * @method static bool removeCondition(void $name)
+ * @method static void clearConditions()
+ * @method static void condition(void $condition)
+ * @method static void loadConditions()
+ * @method static void loadCondition(\Igniter\Cart\CartCondition $condition)
+ * @method static void clearContent()
+ * @method static void store(mixed $identifier)
+ * @method static void restore(mixed $identifier)
+ * @method static void deleteStored(void $identifier)
+ * @method static void keepSession(\Closure $callback)
+ *
+ * @see \Igniter\Cart\Cart
+ */
 class Cart extends Facade
 {
     /**
