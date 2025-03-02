tastyigniter/ti-ext-reservation

Refactor location options as registered location settings
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -5,9 +5,11 @@ namespace Igniter\Reservation;
 use Igniter\Admin\Models\StatusHistory;
 use Igniter\Admin\Widgets\Form;
 use Igniter\Local\Models\Location;
+use Igniter\Local\Models\Location as LocationModel;
 use Igniter\Reservation\Classes\BookingManager;
 use Igniter\Reservation\Listeners\MaxGuestSizePerTimeslotReached;
 use Igniter\Reservation\Listeners\SendReservationConfirmation;
+use Igniter\Reservation\Models\Concerns\LocationAction;
 use Igniter\Reservation\Models\DiningArea;
 use Igniter\Reservation\Models\DiningTable;
 use Igniter\Reservation\Models\Observers\DiningTableObserver;
@@ -16,7 +18,6 @@ use Igniter\Reservation\Models\Reservation;
 use Igniter\Reservation\Models\Scopes\DiningTableScope;
 use Igniter\Reservation\Models\Scopes\ReservationScope;
 use Igniter\Reservation\Requests\ReservationSettingsRequest;
-use Igniter\Reservation\Subscribers\DefineOptionsFormFieldsSubscriber;
 use Igniter\System\Models\Settings;
 use Igniter\User\Http\Controllers\Customers;
 use Igniter\User\Models\Customer;
@@ -32,10 +33,6 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         'igniter.reservation.confirmed' => [
             SendReservationConfirmation::class,
         ],
-    ];
-
-    protected $subscribe = [
-        DefineOptionsFormFieldsSubscriber::class,
     ];
 
     protected $observers = [
@@ -102,6 +99,8 @@ class Extension extends \Igniter\System\Classes\BaseExtension
                 ],
             ], 'primary');
         });
+
+        LocationModel::implement(LocationAction::class);
 
         Location::extend(function (Location $model) {
             $model->relation['hasMany']['dining_areas'] = [DiningArea::class, 'delete' => true];
@@ -228,6 +227,20 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         return [
             \Igniter\Reservation\BulkActionWidgets\AssignTable::class => [
                 'code' => 'assign_table',
+            ],
+        ];
+    }
+
+    public function registerLocationSettings()
+    {
+        return [
+            'booking' => [
+                'label' => 'igniter.reservation::default.settings.text_tab_booking',
+                'description' => 'igniter.reservation::default.settings.text_tab_desc_booking',
+                'icon' => 'fa fa-sliders',
+                'priority' => 0,
+                'form' => 'igniter.reservation::/models/bookingsettings',
+                'request' => \Igniter\Reservation\Requests\BookingSettingsRequest::class,
             ],
         ];
     }

tastyigniter/ti-ext-cart

src/Extension.php
@@ -4,6 +4,7 @@ namespace Igniter\Cart;
 
 use Igniter\Admin\Widgets\Form;
 use Igniter\Cart\Models\Category;
+use Igniter\Cart\Models\Concerns\LocationAction;
 use Igniter\Cart\Models\Menu;
 use Igniter\Cart\Models\MenuItemOption;
 use Igniter\Cart\Models\Observers\MenuItemOptionObserver;
@@ -16,6 +17,7 @@ use Igniter\Cart\Models\Scopes\OrderScope;
 use Igniter\Cart\Requests\OrderSettingsRequest;
 use Igniter\Flame\Igniter;
 use Igniter\Local\Facades\Location;
+use Igniter\Local\Models\Location as LocationModel;
 use Igniter\System\Classes\BaseExtension;
 use Igniter\System\Models\Settings;
 use Igniter\User\Facades\Auth;
@@ -82,9 +84,7 @@ class Extension extends BaseExtension
         $this->bindCheckoutEvents();
         $this->bindOrderStatusEvent();
 
-        Order::extend(function ($model) {
-            $model->implement[] = \Igniter\Cart\Actions\OrderAction::class;
-        });
+        LocationModel::implement(LocationAction::class);
 
         Customers::extendFormFields(function (Form $form) {
             if (!$form->model instanceof Customer) {
@@ -322,6 +322,36 @@ class Extension extends BaseExtension
                 'label' => 'Stock Editor',
                 'code' => 'stockeditor',
             ],
+        ];
+    }
+
+    public function registerLocationSettings()
+    {
+        return [
+            'checkout' => [
+                'label' => 'igniter.cart::default.settings.text_tab_checkout',
+                'description' => 'igniter.cart::default.settings.text_tab_desc_checkout',
+                'icon' => 'fa fa-sliders',
+                'priority' => 0,
+                'form' => 'igniter.cart::/models/checkoutsettings',
+                'request' => \Igniter\Cart\Requests\CheckoutSettingsRequest::class,
+            ],
+            'delivery' => [
+                'label' => 'igniter.cart::default.settings.text_tab_delivery',
+                'description' => 'igniter.cart::default.settings.text_tab_desc_delivery',
+                'icon' => 'fa fa-sliders',
+                'priority' => 0,
+                'form' => 'igniter.cart::/models/deliverysettings',
+                'request' => \Igniter\Cart\Requests\DeliverySettingsRequest::class,
+            ],
+            'collection' => [
+                'label' => 'igniter.cart::default.settings.text_tab_collection',
+                'description' => 'igniter.cart::default.settings.text_tab_desc_collection',
+                'icon' => 'fa fa-sliders',
+                'priority' => 0,
+                'form' => 'igniter.cart::/models/collectionsettings',
+                'request' => \Igniter\Cart\Requests\CollectionSettingsRequest::class,
+            ]
         ];
     }
 