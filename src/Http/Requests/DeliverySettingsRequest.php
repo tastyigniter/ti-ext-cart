8452e6164fedff9b9c5ea1a6c6dbddad61a767ba
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-06-01 17:22:38 +0100

    fix(requests): update delivery and collection settings field names and keys
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Http/Requests/DeliverySettingsRequest.php
+++ src/Http/Requests/DeliverySettingsRequest.php
@@ -14,15 +14,15 @@ class DeliverySettingsRequest extends FormRequest
     {
         return [
             'is_enabled' => lang('igniter.local::default.label_is_enabled'),
-            'delivery_time_interval' => lang('igniter.local::default.label_delivery_time_interval'),
-            'delivery_lead_time' => lang('igniter.local::default.label_delivery_lead_time'),
-            'future_orders.enable_delivery' => lang('igniter.local::default.label_future_delivery_order'),
-            'future_orders.min_delivery_days' => lang('igniter.local::default.label_future_min_delivery_days'),
-            'future_orders.delivery_days' => lang('igniter.local::default.label_future_delivery_days'),
-            'delivery_time_restriction' => lang('igniter.local::default.label_delivery_time_restriction'),
-            'delivery_cancellation_timeout' => lang('igniter.local::default.label_delivery_cancellation_timeout'),
-            'delivery_add_lead_time' => lang('igniter.local::default.label_delivery_add_lead_time'),
-            'delivery_min_order_amount' => lang('igniter.local::default.label_delivery_min_order_amount'),
+            'add_lead_time' => lang('igniter.local::default.label_delivery_add_lead_time'),
+            'time_interval' => lang('igniter.local::default.label_delivery_time_interval'),
+            'lead_time' => lang('igniter.local::default.label_delivery_lead_time'),
+            'time_restriction' => lang('igniter.local::default.label_delivery_time_restriction'),
+            'cancellation_timeout' => lang('igniter.local::default.label_delivery_cancellation_timeout'),
+            'min_order_amount' => lang('igniter.local::default.label_delivery_min_order_amount'),
+            'future_orders.is_enabled' => lang('igniter.local::default.label_future_delivery_order'),
+            'future_orders.min_days' => lang('igniter.local::default.label_future_min_delivery_days'),
+            'future_orders.days' => lang('igniter.local::default.label_future_delivery_days'),
         ];
     }
 
@@ -30,15 +30,15 @@ class DeliverySettingsRequest extends FormRequest
     {
         return [
             'is_enabled' => ['boolean'],
-            'delivery_time_interval' => ['integer', 'min:5'],
-            'delivery_lead_time' => ['integer', 'min:5'],
-            'future_orders.enable_delivery' => ['boolean'],
-            'future_orders.min_delivery_days' => ['integer', 'min:0'],
-            'future_orders.delivery_days' => ['integer', 'min:0', 'gt:future_orders.min_delivery_days'],
-            'delivery_time_restriction' => ['nullable', 'integer', 'max:2'],
-            'delivery_add_lead_time' => ['boolean'],
-            'delivery_cancellation_timeout' => ['integer', 'min:0', 'max:999'],
-            'delivery_min_order_amount' => ['numeric', 'min:0'],
+            'add_lead_time' => ['boolean'],
+            'time_interval' => ['integer', 'min:5'],
+            'lead_time' => ['integer', 'min:5'],
+            'time_restriction' => ['nullable', 'integer', 'max:2'],
+            'cancellation_timeout' => ['integer', 'min:0', 'max:999'],
+            'min_order_amount' => ['numeric', 'min:0'],
+            'future_orders.is_enabled' => ['boolean'],
+            'future_orders.min_days' => ['integer', 'min:0'],
+            'future_orders.days' => ['integer', 'min:0', 'gt:future_orders.min_days'],
         ];
     }
 }
